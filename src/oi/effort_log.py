5ea782313e4c588301df44b23fad9034f9b6e3f8
Alex Zanfir <alexzanfir@gmail.com>
2026-02-09 17:37:08 +0700

    Add effort_log.py stubs for Story 3 tests

---
+++ src/oi/effort_log.py
@@ -0,0 +1,5 @@
+"""Stub module: oi.effort_log (auto-generated for TDD)."""
+
+def append_to_effort_log(session_dir, effort_id, user_message):
+    raise NotImplementedError('append_to_effort_log')
+
